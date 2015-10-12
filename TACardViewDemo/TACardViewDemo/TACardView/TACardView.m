d2714dde70031499651104d7a03bdc13bc95e6f1
zip lee <54459708@qq.com>
2015-10-12 17:05:29 +0800

    back up

--- TACardViewDemo/TACardViewDemo/TACardView/TACardView.m
+++ TACardViewDemo/TACardViewDemo/TACardView/TACardView.m
@@ -93,9 +93,18 @@ IB_DESIGNABLE @implementation TACardView
     if (_previewIndexArray.count > 0) {
         [_previewIndexArray removeObjectAtIndex:0];
     }
+    [UIView animateWithDuration:0.5 animations:^{
+        [_containerView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSUInteger i = _containerView.subviews.count - idx - 1;
+            obj.frame = CGRectMake(0 + _edgeOffset * i, 0 + _edgeOffset * 3 * i, self.frame.size.width - _edgeOffset * 2 * i, self.frame.size.height - _edgeOffset * 2 * i);
+        }];
+    }];
     if ([_dataSource respondsToSelector:@selector(cardView:viewAtIndex:)]) {
         NSUInteger nextIndex = _previewIndexArray.lastObject.unsignedIntegerValue + 1;
         UIView* subcard = [_dataSource cardView:self viewAtIndex: nextIndex];
+        NSUInteger i = _numberOfViewsPreview - 1;
+        subcard.frame = CGRectMake(0 + _edgeOffset * i, 0 + _edgeOffset * 3 * i, self.frame.size.width - _edgeOffset * 2 * i, self.frame.size.height - _edgeOffset * 2 * i);
+
         [_containerView addSubview:subcard];
         [_containerView sendSubviewToBack:subcard];
         [_previewIndexArray addObject:@(nextIndex)];
